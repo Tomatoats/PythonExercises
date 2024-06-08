869b23ec13276d175203ad00c6cba1fc46f9c3ac
Tomatoats <alexys.veloz.fl@gmail.com>
2024-06-07 20:41:29 -0400

    finally updated to new python. 21 done.

--- Python Exercises/Exercise20-29/ex21.py
+++ Python Exercises/Exercise20-29/ex21.py
@@ -1,12 +1,47 @@
-flag1 = False
-while flag1 == False:
+flag = False
+month = ""
+while flag == False:
     number = input("Please enter the number of the month ")
-    try:
-        int(number)
-    except ValueError:
-        print("ayo mate i need numbers.")
-    
-    else:
-        flag1 = True
 
-    
+    match number:
+        case '1':
+            month = "January"
+            flag = True
+        case '2':
+            month = "February"
+            flag = True
+        case '3':
+            month = "March"
+            flag = True
+        case '4':
+            month = "April"
+            flag = True
+        case '5':
+            month = "May"
+            flag = True
+        case '6':
+            month = "June"
+            flag = True
+        case '7':
+            month = "July"
+            flag = True
+        case '8':
+            month = "August"
+            flag = True
+        case '9':
+            month = "September"
+            flag = True
+        case '10':
+            month = "October"
+            flag = True
+        case '11':
+            month = "November"
+            flag = True
+        case '12':
+            month = "December"
+            flag = True
+        case _:
+            flag = False
+            print("Please give me a number inbetween 1 and 12.")
+output = "The name of the month is {0}."
+print(output.format(month))
