0fd6a8b5a98eb7fcc9417ac3ff14e55281ae8ad5
Alexys Veloz <alexys.veloz.fl@gmail.com>
2024-07-08 15:14:51 -0400

    40 partial done, added the other series

---
+++ Python Exercises/Exercise40-50/ex40.py
@@ -0,0 +1,40 @@
+from prettytable import PrettyTable
+def Person(num):
+    id0 = ["John Johnson","Manager","2016-12-31"]
+    id1 = ["Tou Xiong","Software Engineer","2016-10-05"]
+    id2 = ["Michaela Michaelson","District Manager","2015-12-19"]
+    id3 = ["Jake Jacobson","Programmer",""]
+    id4 = ["Jacquelyn Jackson","DBA",""]
+    id5 = ["Sally Webber","Web Deveolper","2015-12-18"]
+    match num:
+        case 0: return id0
+        case 1: return id1
+        case 2: return id2
+        case 3: return id3 
+        case 4: return id4
+        case 5: return id5
+
+def results(toFind):
+    pass
+
+
+def printTable(total):
+    table = PrettyTable()
+    table.field_names = ["Name","Position","Seperation Date"]
+    for x in total:
+        table.add_row(x)
+    print(table)
+
+
+num = 5
+total = list(map(Person, range(num)))
+
+printTable(total)
+
+toFind = input("Enter a search string ")
+
+print("Results:")
+
+results(toFind,)
+
+    
